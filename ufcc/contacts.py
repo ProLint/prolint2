23c13ad2a95ac83f16cf1e3d4a4d2757b9c0fab2
dpastor <daniel.ramirezecheme@ucalgary.ca>
2022-05-24 08:36:53 -0600

    cleaning contacts output

--- ufcc/contacts.py
+++ ufcc/contacts.py
@@ -2,6 +2,8 @@
 # Prolint: A tool to analyze and visualize lipid-protein interactions.
 #
 
+
+import numpy as np
 import MDAnalysis as mda
 from MDAnalysis.lib.nsgrid import FastNS
 from multiprocessing import Pool
@@ -19,8 +21,8 @@ class Contacts(object):
         The whole system.
     query : MDA.AtomGroup 
         Query atoms.
-    haystack : MDA.AtomGroup 
-        Haystack atoms.
+    database : MDA.AtomGroup 
+        database atoms.
 
     Methods
     -------
@@ -28,7 +30,7 @@ class Contacts(object):
         Return the index pairs for the contacts on each frame.
     """
 
-    def __init__(self, UFCC_universe, query, haystacks):
+    def __init__(self, UFCC_universe, query, database):
         """
         Parameters
         ---------
@@ -36,19 +38,31 @@ class Contacts(object):
             The whole system.
         query : MDA.AtomGroup 
             Query atoms.
-        haystack : MDA.AtomGroup 
-            Haystack atoms.
+        database : MDA.AtomGroup 
+            Database atoms.
         """
         self.u = UFCC_universe
         self.query = query
-        self.haystacks = haystacks
+        self.database = database
 
     def per_block(self, blocksize):
         results = {}
         for ts in self.u.trajectory[blocksize.start:blocksize.stop]:
-            gridsearch = FastNS(7, self.query.positions, box=self.query.dimensions, pbc=True)
-            result = gridsearch.search(self.haystacks.positions)
-            results[ts.frame] = result.get_pairs()
+            gridsearch = FastNS(7, self.database.positions, box=self.database.dimensions, pbc=True)
+            result = gridsearch.search(self.query.positions)
+            pairs = result.get_pairs()
+            contacts_per_idx = []
+            flag = 0
+            for pair_idx in range(len(pairs)):
+                if pair_idx == 0:
+                    contacts_per_idx.append([pairs[pair_idx][0], pairs[pair_idx][1]])
+                else:
+                    if pairs[pair_idx][0] == pairs[pair_idx-1][0]:
+                        contacts_per_idx[flag].append(pairs[pair_idx][1])
+                    else:
+                        flag += 1
+                        contacts_per_idx.append([pairs[pair_idx][0], pairs[pair_idx][1]])
+            results[ts.frame] = contacts_per_idx
         return results
 
     def make_blocks(self, n_blocks):
@@ -59,7 +73,7 @@ class Contacts(object):
         return blocks
 
     def run(self, n_jobs):
-        self.u.transfer_to_memory(verbose=True)
+        #self.u.transfer_to_memory(verbose=True)
         blocks = self.make_blocks(n_jobs)
         with Pool(processes=n_jobs) as worker_pool:
             all_result = worker_pool.map(self.per_block, blocks)
@@ -73,6 +87,9 @@ class Contacts(object):
         get_contacts(n_jobs)
             Return the index pairs for the contacts on each frame.
         """
-        contacts = self.run(n_jobs)
-        return contacts
+        dirty_contacts = self.run(n_jobs)
+        clean_contacts = {}
+        for block in dirty_contacts:
+            clean_contacts = {**clean_contacts, **block}
+        return clean_contacts
         # TODO: create tool to process results
