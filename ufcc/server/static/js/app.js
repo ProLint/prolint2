d58e86f39b6ec9113ab22b9ab9773f104e5e8c9d
Besian Sejdiu <besiansejdiu@gmail.com>
2022-08-07 19:50:11 -0500

    initial setup of pie chart

--- ufcc/server/static/js/app.js
+++ ufcc/server/static/js/app.js
@@ -322,7 +322,250 @@ fetch('/data/' + JSON.stringify(obj))
                 });
             series.appear(1000);
             chart.appear(500, 100);
-
         });
+
+
+        var pieRoot = am5.Root.new("chartdiv2");
+
+        // Set themes
+        // https://www.amcharts.com/docs/v5/concepts/themes/
+        pieRoot.setThemes([am5themes_Animated.new(pieRoot)]);
+
+        var pieContainer = pieRoot.container.children.push(
+            am5.Container.new(pieRoot, {
+              width: am5.p100,
+              height: am5.p100,
+              layout: pieRoot.horizontalLayout
+            })
+          );
+
+          // Create main chart
+          // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+          var pieChart = pieContainer.children.push(
+            am5percent.PieChart.new(pieRoot, {
+              tooltip: am5.Tooltip.new(pieRoot, {})
+            })
+          );
+
+          // Create series
+          // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+          var pieSeries = pieChart.series.push(
+            am5percent.PieSeries.new(pieRoot, {
+              valueField: "value",
+              categoryField: "category",
+              alignLabels: false
+            })
+          );
+
+          pieSeries.labels.template.setAll({
+            textType: "circular",
+            radius: 4
+          });
+          pieSeries.ticks.template.set("visible", false);
+          pieSeries.slices.template.set("toggleKey", "none");
+
+          // add events
+          pieSeries.slices.template.events.on("click", function(e) {
+            selectSlice(e.target);
+          });
+
+          // Create sub chart
+          // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+          var subChart = pieContainer.children.push(
+            am5percent.PieChart.new(pieRoot, {
+              radius: am5.percent(50),
+              tooltip: am5.Tooltip.new(pieRoot, {})
+            })
+          );
+
+          // Create sub series
+          // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+          var subSeries = subChart.series.push(
+            am5percent.PieSeries.new(pieRoot, {
+              valueField: "value",
+              categoryField: "category"
+            })
+          );
+
+          subSeries.data.setAll([
+            { category: "A", value: 0 },
+            { category: "B", value: 0 },
+            { category: "C", value: 0 },
+            { category: "D", value: 0 },
+            { category: "E", value: 0 },
+            { category: "F", value: 0 },
+            { category: "G", value: 0 }
+          ]);
+          subSeries.slices.template.set("toggleKey", "none");
+
+          var selectedSlice;
+
+          pieSeries.on("startAngle", function() {
+            updateLines();
+          });
+
+          pieContainer.events.on("boundschanged", function() {
+            pieRoot.events.on("frameended", function(){
+              updateLines();
+             })
+          })
+
+          function updateLines() {
+            if (selectedSlice) {
+              var startAngle = selectedSlice.get("startAngle");
+              var arc = selectedSlice.get("arc");
+              var radius = selectedSlice.get("radius");
+
+              var x00 = radius * am5.math.cos(startAngle);
+              var y00 = radius * am5.math.sin(startAngle);
+
+              var x10 = radius * am5.math.cos(startAngle + arc);
+              var y10 = radius * am5.math.sin(startAngle + arc);
+
+              var subRadius = subSeries.slices.getIndex(0).get("radius");
+              var x01 = 0;
+              var y01 = -subRadius;
+
+              var x11 = 0;
+              var y11 = subRadius;
+
+              var point00 = pieSeries.toGlobal({ x: x00, y: y00 });
+              var point10 = pieSeries.toGlobal({ x: x10, y: y10 });
+
+              var point01 = subSeries.toGlobal({ x: x01, y: y01 });
+              var point11 = subSeries.toGlobal({ x: x11, y: y11 });
+
+              line0.set("points", [point00, point01]);
+              line1.set("points", [point10, point11]);
+            }
+          }
+
+          // lines
+          var line0 = pieContainer.children.push(
+            am5.Line.new(pieRoot, {
+              position: "absolute",
+              stroke: pieRoot.interfaceColors.get("text"),
+              strokeDasharray: [2, 2]
+            })
+          );
+          var line1 = pieContainer.children.push(
+            am5.Line.new(pieRoot, {
+              position: "absolute",
+              stroke: pieRoot.interfaceColors.get("text"),
+              strokeDasharray: [2, 2]
+            })
+          );
+
+          // Set data
+          // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+          pieSeries.data.setAll([
+            {
+              category: "Lithuania",
+              value: 500,
+              subData: [
+                { category: "A", value: 200 },
+                { category: "B", value: 150 },
+                { category: "C", value: 100 },
+                { category: "D", value: 100 }
+              ]
+            },
+            {
+              category: "Czechia",
+              value: 300,
+              subData: [
+                { category: "A", value: 150 }
+              ]
+            },
+            {
+              category: "Ireland",
+              value: 200,
+              subData: [
+                { category: "A", value: 110 },
+                { category: "B", value: 60 },
+                { category: "C", value: 30 }
+              ]
+            },
+            {
+              category: "Germany",
+              value: 150,
+              subData: [
+                { category: "A", value: 80 },
+                { category: "B", value: 40 },
+                { category: "C", value: 30 }
+              ]
+            },
+            {
+              category: "Australia",
+              value: 140,
+              subData: [
+                { category: "A", value: 90 },
+                { category: "B", value: 40 },
+                { category: "C", value: 10 }
+              ]
+            },
+            {
+              category: "Austria",
+              value: 120,
+              subData: [
+                { category: "A", value: 60 },
+                { category: "B", value: 30 },
+                { category: "C", value: 30 }
+              ]
+            }
+          ]);
+
+          function selectSlice(slice) {
+            selectedSlice = slice;
+            var dataItem = slice.dataItem;
+            var dataContext = dataItem.dataContext;
+
+            if (dataContext) {
+              var i = 0;
+              subSeries.data.each(function(dataObject) {
+                var dataObj = dataContext.subData[i];
+                if(dataObj){
+                    subSeries.data.setIndex(i, dataObj);
+                    if(!subSeries.dataItems[i].get("visible")){
+                        subSeries.dataItems[i].show();
+                    }
+                }
+                else{
+                    subSeries.dataItems[i].hide();
+                }
+
+                i++;
+              });
+            }
+
+            var middleAngle = slice.get("startAngle") + slice.get("arc") / 2;
+            var firstAngle = pieSeries.dataItems[0].get("slice").get("startAngle");
+
+            pieSeries.animate({
+              key: "startAngle",
+              to: firstAngle - middleAngle,
+              duration: 1000,
+              easing: am5.ease.out(am5.ease.cubic)
+            });
+            pieSeries.animate({
+              key: "endAngle",
+              to: firstAngle - middleAngle + 360,
+              duration: 1000,
+              easing: am5.ease.out(am5.ease.cubic)
+            });
+          }
+
+          pieContainer.appear(1000, 10);
+
+          pieSeries.events.on("datavalidated", function() {
+            selectSlice(pieSeries.slices.getIndex(0));
+          });
+
+
+
+
+
+
+
+
 
     });
