022d682e7bbdb827c90018a16d7e76082af1bb4b
Daniel Pastor Ramirez <daniel.ramirezecheme@ucalgary.ca>
2022-09-05 10:46:17 -0600

    adding two attributes to the Contacts class

--- ufcc/contacts.py
+++ ufcc/contacts.py
@@ -221,6 +221,8 @@ class Contacts(object):
         self.runner = Runner()
         self.cutoff = None
         self.contacts = None
+        self.contacts_sum = None
+        self.contact_frames = None
 
         # TODO:
         # @bis: I really don't like how we have to back reference the trajectory here
