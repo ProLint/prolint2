20b43cba8b7e642b279bad925ef8701fdc47b5a8
Daniel Pastor Ramirez <daniel.ramirezecheme@ucalgary.ca>
2023-08-08 09:03:45 -0600

    plot residue logo

--- prolint2/plot/plot_1d.py
+++ prolint2/plot/plot_1d.py
@@ -1,12 +1,16 @@
 import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 import inspect
-from .utils import use_1d_script_template, get_metric_list_by_residues
+from .utils import *
+import logomaker
 
 
-##### ALL THE PLOTS ARE GOING TO BE GENERATED DIRECTLY FROM A UNIVERSE AND A CONTACTS INSTANCE 
+##### ALL THE PLOTS ARE GOING TO BE GENERATED DIRECTLY FROM A UNIVERSE AND A CONTACTS INSTANCE
 # THAT CAN BE LOADED FROM A CSV FILE OR CALCULATED FROM SCRATCH #####
+
 
 class Plotter1D:
     def __init__(self, xlabel=None, ylabel=None, fn=None, title=None, fig_close=False):
@@ -29,28 +33,41 @@ class Plotter1D:
     def generate_script(self, class_code, script_filename):
         # create a python script that generates the plot
         plotting_function_source = inspect.getsource(class_code)
-        if self.__class__.__name__ == 'ResiduePlot':
+        if self.__class__.__name__ in ["ResiduePlot", "ResidueLogo"]:
             tail = """
     # Generate the plot
     PLOT = {}(u, mean_contacts, lipid='CHOL', metric_name='MeanMetric')
     PLOT.save_plot(show=False)
-            """.format(self.__class__.__name__)
+            """.format(
+                self.__class__.__name__
+            )
         script_code = use_1d_script_template(plotting_function_source, tail)
 
-        with open(script_filename, 'w') as f:
+        with open(script_filename, "w") as f:
             f.write(script_code)
 
 
 class ResiduePlot(Plotter1D):
-    def __init__(self, universe, metric, lipid=None, metric_name=None, xlabel=None, ylabel=None, fn=None, title=None, fig_close=False, generate_script=False):
+    def __init__(
+        self,
+        universe,
+        metric,
+        lipid=None,
+        metric_name=None,
+        xlabel=None,
+        ylabel=None,
+        fn=None,
+        title=None,
+        fig_close=False,
+        generate_script=False,
+    ):
         super().__init__(xlabel, ylabel, fn, title, fig_close)
         self.resids = universe.query.residues.resids
         self.metric = get_metric_list_by_residues(universe, metric, lipid, metric_name)
 
-        
-    def create_plot(self):    
+    def create_plot(self):
         """Plot interactions as a function of residue index"""
-        
+
         bar_color = "#176BA0"
         if self.ylabel is None:
             self.ylabel = "Interactions"
@@ -65,10 +82,12 @@ class ResiduePlot(Plotter1D):
             300: (3.8, 1.8),
             1000: (4.5, 1.8),
             2000: (6.0, 1.8),
-            np.inf: (7.5, 1.8)
+            np.inf: (7.5, 1.8),
         }
 
-        size = next(size for limit, size in size_mapping.items() if len(self.resids) <= limit)
+        size = next(
+            size for limit, size in size_mapping.items() if len(self.resids) <= limit
+        )
         figsize = size_mapping.get(size, (7.5, 1.8))
 
         self.figure, self.axis = plt.subplots(1, 1, figsize=figsize)
@@ -80,7 +99,9 @@ class ResiduePlot(Plotter1D):
         self.axis.set_ylabel(self.ylabel, fontsize=8, weight="bold")
         self.axis.set_xlabel("Residue Index", fontsize=8, weight="bold")
 
-        for label in self.axis.xaxis.get_ticklabels() + self.axis.yaxis.get_ticklabels():
+        for label in (
+            self.axis.xaxis.get_ticklabels() + self.axis.yaxis.get_ticklabels()
+        ):
             label.set_fontsize(8)
             label.set_weight("bold")
 
@@ -90,7 +111,114 @@ class ResiduePlot(Plotter1D):
         plt.tight_layout()
 
 
-    
+class ResidueLogo(Plotter1D):
+    def __init__(
+        self,
+        universe,
+        metric,
+        lipid=None,
+        metric_name=None,
+        xlabel=None,
+        ylabel=None,
+        fn=None,
+        title=None,
+        fig_close=False,
+        generate_script=False,
+    ):
+        super().__init__(xlabel, ylabel, fn, title, fig_close)
+        self.resids = universe.query.residues.resids
+        self.logos = universe.query.residues.resnames
+        self.metric = get_metric_list_by_residues(universe, metric, lipid, metric_name)
 
+    def create_plot(self):
+        letter_map = {
+            "CYS": "C",
+            "ASP": "D",
+            "SER": "S",
+            "GLN": "Q",
+            "LYS": "K",
+            "ILE": "I",
+            "PRO": "P",
+            "THR": "T",
+            "PHE": "F",
+            "ASN": "N",
+            "GLY": "G",
+            "HIS": "H",
+            "LEU": "L",
+            "ARG": "R",
+            "TRP": "W",
+            "ALA": "A",
+            "VAL": "V",
+            "GLU": "E",
+            "TYR": "Y",
+            "MET": "M",
+        }
 
+        color_scheme = "chemistry"
 
+        logos_checked = []
+        for name in self.logos:
+            if len(name) == 1:
+                logos_checked.append(name)
+            else:
+                logos_checked.append(letter_map[name])
+        if self.ylabel is None:
+            self.ylabel = "Interactions"
+        if self.fn is None:
+            self.fn = os.path.join(os.getcwd(), "Figure_interactions_logo.pdf")
+
+        length = 100
+        gap = 1000
+        # check for chain breaks, gray_areas and axis breaks
+        axis_obj = AxisIndex(self.resids, logos_checked, self.metric, length, gap)
+        axis_obj.sort()
+        # plot
+        for page_idx in axis_obj.breaks.keys():
+            n_rows = len(axis_obj.breaks[page_idx])
+            fig, axes = plt.subplots(
+                n_rows, 1, figsize=(4.5, 1.3 * n_rows), sharey=True
+            )
+            plt.subplots_adjust(hspace=0.5, left=0.2)
+            ymax = []
+            for ax_idx, ax in enumerate(np.atleast_1d(axes)):
+                resi_selected = [item[0] for item in axis_obj.breaks[page_idx][ax_idx]]
+                logos_selected = [item[1] for item in axis_obj.breaks[page_idx][ax_idx]]
+                interaction_selected = [
+                    item[2] for item in axis_obj.breaks[page_idx][ax_idx]
+                ]
+                ymax.append(np.max(interaction_selected))
+                if np.sum(interaction_selected) > 0:
+                    df = pd.DataFrame(
+                        {
+                            "Resid": resi_selected,
+                            "Resn": logos_selected,
+                            "Data": interaction_selected,
+                        }
+                    )
+                    matrix = df.pivot(
+                        index="Resid", columns="Resn", values="Data"
+                    ).fillna(0)
+                    logomaker.Logo(matrix, color_scheme=color_scheme, ax=ax)
+                if ax_idx == (n_rows - 1):
+                    ax.set_xlabel("Residue Index", fontsize=8, weight="bold")
+                ax.xaxis.set_major_locator(MultipleLocator(20))
+                ax.xaxis.set_minor_locator(MultipleLocator(1))
+                ax.set_xlim(resi_selected[0] - 0.5, resi_selected[-1] + 0.5)
+                ax.set_ylabel(self.ylabel, fontsize=8, weight="bold", va="center")
+                for label in ax.xaxis.get_ticklabels() + ax.yaxis.get_ticklabels():
+                    plt.setp(label, fontsize=8, weight="bold")
+            np.atleast_1d(axes)[-1].set_ylim(0, np.max(ymax) * 1.05)
+            # plot missing areas
+            if page_idx in axis_obj.gray_areas.keys():
+                for item in axis_obj.gray_areas[page_idx]:
+                    np.atleast_1d(axes)[item[0]].axvspan(
+                        item[1], item[2], facecolor="#c0c0c0", alpha=0.3
+                    )
+            if self.title is not None:
+                np.atleast_1d(axes)[0].set_title(self.title, fontsize=10, weight="bold")
+            plt.tight_layout()
+            if len(axis_obj.breaks.keys()) == 1:
+                fig.savefig(self.fn, dpi=300)
+            else:
+                name, ext = os.path.splitext(self.fn)
+                fig.savefig("{}_{}{}".format(name, page_idx, ext), dpi=300)
--- prolint2/plot/utils.py
+++ prolint2/plot/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 
 
 def use_1d_script_template(code_body, tail):
@@ -8,7 +9,10 @@ def use_1d_script_template(code_body, tail):
 
 import os
 import numpy as np
+import pandas as pd
+import logomaker
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 from prolint2 import Universe
 from prolint2.plot.plot_1d import Plotter1D
 from prolint2.plot.utils import *
@@ -28,8 +32,11 @@ if __name__ == "__main__":
     mean_contacts = metric_instance.compute()
     mean_contacts
 {}
-    """.format(code_body, tail)
+    """.format(
+        code_body, tail
+    )
     return template
+
 
 def get_metric_list_by_residues(universe, metric, lipid=None, metric_name=None):
     """Get a list of metric values for a given lipid and metric name"""
@@ -45,20 +52,37 @@ def get_metric_list_by_residues(universe, metric, lipid=None, metric_name=None):
                         metric_list.append(0)
                 else:
                     # get the lipid names available
-                    lipids_dict = {i: lipid for i, lipid in enumerate(universe.database.unique_resnames)}
-                    lipid = int(input("Please enter the lipid name that you want to analyze: \n {}.".format(lipids_dict)))
+                    lipids_dict = {
+                        i: lipid
+                        for i, lipid in enumerate(universe.database.unique_resnames)
+                    }
+                    lipid = int(
+                        input(
+                            "Please enter the lipid name that you want to analyze: \n {}.".format(
+                                lipids_dict
+                            )
+                        )
+                    )
                     try:
                         lipid = lipids_dict[lipid]
                     except KeyError:
                         print("The lipid {} is not available".format(lipid))
                     if metric_name not in metric[resi][lipid].keys():
                         # break the loops and print error message
-                        print("The metric {} is not available for the lipid {}.".format(metric_name, lipid))
+                        print(
+                            "The metric {} is not available for the lipid {}.".format(
+                                metric_name, lipid
+                            )
+                        )
                         break
                     try:
                         metric_list.append(metric[resi][lipid][metric_name])
                     except KeyError:
-                        print("The metric {} is not available for the lipid {}.".format(metric_name, lipid))
+                        print(
+                            "The metric {} is not available for the lipid {}.".format(
+                                metric_name, lipid
+                            )
+                        )
             else:
                 if lipid:
                     if lipid not in universe.database.unique_resnames:
@@ -67,8 +91,17 @@ def get_metric_list_by_residues(universe, metric, lipid=None, metric_name=None):
                         break
                 else:
                     # get the lipid names available
-                    lipids_dict = {i: lipid for i, lipid in enumerate(universe.database.unique_resnames)}
-                    lipid = int(input("Please enter the lipid name that you want to analyze: \n {}.".format(lipids_dict)))
+                    lipids_dict = {
+                        i: lipid
+                        for i, lipid in enumerate(universe.database.unique_resnames)
+                    }
+                    lipid = int(
+                        input(
+                            "Please enter the lipid name that you want to analyze: \n {}.".format(
+                                lipids_dict
+                            )
+                        )
+                    )
                     try:
                         lipid = lipids_dict[lipid]
                     except KeyError:
@@ -77,10 +110,18 @@ def get_metric_list_by_residues(universe, metric, lipid=None, metric_name=None):
                 metric_names = list(metric[resi][lipid].keys())
                 inc = 1
                 while len(metric_names) == 0:
-                    metric_names = list(metric[resi+inc][lipid].keys())
+                    metric_names = list(metric[resi + inc][lipid].keys())
                     inc += 1
-                metric_names_dict = {i: metric_name for i, metric_name in enumerate(metric_names)}
-                metric_name = int(input("Please enter the metric name that you want to analyze: \n {}.".format(metric_names_dict)))
+                metric_names_dict = {
+                    i: metric_name for i, metric_name in enumerate(metric_names)
+                }
+                metric_name = int(
+                    input(
+                        "Please enter the metric name that you want to analyze: \n {}.".format(
+                            metric_names_dict
+                        )
+                    )
+                )
                 try:
                     metric_name = metric_names_dict[metric_name]
                 except KeyError:
@@ -92,3 +133,136 @@ def get_metric_list_by_residues(universe, metric, lipid=None, metric_name=None):
         else:
             metric_list.append(0)
     return np.array(metric_list)
+
+
+class AxisIndex:
+    """Build axes for logo figure."""
+
+    def __init__(self, residue_index, logos, interactions, length, gap):
+        self.page_idx = 0
+        self.length = length
+        self.gap = gap
+        self.residue_index = residue_index
+        self.logos = logos
+        self.interactions = interactions
+        self.axis_start = (residue_index[0] // length) * length
+        self.breaks = defaultdict(list)
+        self.breaks[self.page_idx].append([])
+        self.gray_areas = defaultdict(list)
+
+    def fill_missing(self, start_value, end_value):
+        for xloci in np.arange(start_value, end_value + 1):
+            self.breaks[self.page_idx][-1].append((xloci, "A", 0))
+        self.gray_areas[self.page_idx].append(
+            (len(self.breaks[self.page_idx]) - 1, start_value, end_value)
+        )
+
+    def new_axis(self, pointer):
+        self.breaks[self.page_idx].append([])
+        self.axis_start = self.residue_index[pointer]
+        self.breaks[self.page_idx][-1].append(
+            (
+                self.residue_index[pointer],
+                self.logos[pointer],
+                self.interactions[pointer],
+            )
+        )
+
+    def new_page(self, pointer):
+        if len(self.breaks[self.page_idx][-1]) < self.length:
+            self.fill_missing(
+                self.axis_start + len(self.breaks[self.page_idx][-1]),
+                self.axis_start + self.length - 1,
+            )
+        self.page_idx += 1
+        self.breaks[self.page_idx].append([])
+        self.axis_start = (self.residue_index[pointer] // self.length) * self.length
+        if self.axis_start != self.residue_index[pointer]:
+            self.fill_missing(self.axis_start, self.residue_index[pointer] - 1)
+        self.breaks[self.page_idx][-1].append(
+            (
+                self.residue_index[pointer],
+                self.logos[pointer],
+                self.interactions[pointer],
+            )
+        )
+
+    def new_gap(self, pointer):
+        gray_start = self.residue_index[pointer - 1] + 1
+        for xloci in np.arange(
+            self.residue_index[pointer - 1] + 1, self.residue_index[pointer]
+        ):
+            if xloci - self.axis_start < self.length:
+                self.breaks[self.page_idx][-1].append((xloci, "A", 0))
+            else:
+                self.gray_areas[self.page_idx].append(
+                    (len(self.breaks[self.page_idx]) - 1, gray_start, xloci - 1)
+                )
+                self.breaks[self.page_idx].append([])
+                self.breaks[self.page_idx][-1].append((xloci, "A", 0))
+                self.axis_start = xloci
+                gray_start = xloci
+        self.gray_areas[self.page_idx].append(
+            (
+                len(self.breaks[self.page_idx]) - 1,
+                gray_start,
+                self.residue_index[pointer] - 1,
+            )
+        )
+        self.breaks[self.page_idx][-1].append(
+            (
+                self.residue_index[pointer],
+                self.logos[pointer],
+                self.interactions[pointer],
+            )
+        )
+
+    def sort(self):
+        end = False
+        if self.axis_start != self.residue_index[0]:
+            self.fill_missing(self.axis_start, self.residue_index[0] - 1)
+        self.breaks[self.page_idx][-1].append(
+            (self.residue_index[0], self.logos[0], self.interactions[0])
+        )
+        pointer = 1
+        while not end:
+            if (
+                self.residue_index[pointer] - self.residue_index[pointer - 1] == 1
+                and self.residue_index[pointer] - self.axis_start < self.length
+            ):
+                self.breaks[self.page_idx][-1].append(
+                    (
+                        self.residue_index[pointer],
+                        self.logos[pointer],
+                        self.interactions[pointer],
+                    )
+                )
+                pointer += 1
+            elif (
+                self.residue_index[pointer] - self.residue_index[pointer - 1] == 1
+                and self.residue_index[pointer] - self.axis_start >= self.length
+            ):
+                self.new_axis(pointer)
+                pointer += 1
+            elif self.residue_index[pointer] - self.residue_index[pointer - 1] < 0:
+                self.new_page(pointer)
+                pointer += 1
+            elif (
+                1
+                < self.residue_index[pointer] - self.residue_index[pointer - 1]
+                <= self.gap
+            ):
+                self.new_gap(pointer)
+                pointer += 1
+            elif (
+                self.residue_index[pointer] - self.residue_index[pointer - 1] > self.gap
+            ):
+                self.new_page(pointer)
+                pointer += 1
+            if pointer == len(self.residue_index):
+                end = True
+        if len(self.breaks[self.page_idx][-1]) < self.length:
+            self.fill_missing(
+                self.axis_start + len(self.breaks[self.page_idx][-1]),
+                self.axis_start + self.length - 1,
+            )
