90c41084759726f4b727fa564492ff5d15bce2c3
Daniel Pastor Ramirez <daniel.ramirezecheme@ucalgary.ca>
2023-08-17 10:51:31 -0600

    projection completed

--- prolint2/plotting/projection.py
+++ prolint2/plotting/projection.py
@@ -27,11 +27,10 @@ def show_contact_projection(
     max_metric = max(metric_list)
     dat_resnames = universe.database.residues.resnames
     metric_dist2 = {
-        res_id: max_metric
-        if dat_resnames[idx] == lipid else 0
+        res_id: max_metric if dat_resnames[idx] == lipid else 0
         for idx, res_id in enumerate(universe.database.residues.resindices)
     }
-    
+
     metric_dict.update(metric_dist2)
 
     atomic_bfactors = [metric_dict[x] for x in universe.atoms.resindices]
@@ -43,33 +42,50 @@ def show_contact_projection(
 
     scheme = nv.color._ColorScheme(cs, "bf")
 
-    if frame_idx is None:
-        universe.trajectory[0]
-    else:
+    if frame_idx is not None:
         universe.trajectory[frame_idx]
-    universe.query.atoms.write("temp.pdb")
-    universe.database.atoms.write("temp2.pdb")
 
-    with open("temp.pdb", "r") as myfile:
-        with open("temp2.pdb") as myfile2:
-            query_structure = nv.TextStructure(myfile.read())
-            database_structure = nv.TextStructure(myfile2.read())
-            view = nv.NGLWidget()
-            view.add_component(query_structure)
-            view.add_component(database_structure)
-            view.clear_representations(component=0)
-            view.clear_representations(component=1)
-            if query_repr == "surface":
-                view.add_representation("surface", surfaceType="av", probeRadius=2.1, component=0, color=scheme)
-            else:
-                view.add_representation(query_repr, component=0, color=scheme)
-            view.add_representation(database_repr, component=1, color=scheme)
-            view.center(component=0)
+        universe.query.atoms.write("temp.pdb")
+        universe.database.atoms.write("temp2.pdb")
 
-    # close file and remove it
-    myfile.close()
-    myfile2.close()
-    os.remove("temp.pdb")
-    os.remove("temp2.pdb")
+        with open("temp.pdb", "r") as myfile:
+            with open("temp2.pdb") as myfile2:
+                query_structure = nv.TextStructure(myfile.read())
+                database_structure = nv.TextStructure(myfile2.read())
+                view = nv.NGLWidget()
+                view.add_component(query_structure)
+                view.add_component(database_structure)
+                view.clear_representations(component=0)
+                view.clear_representations(component=1)
+                if query_repr == "surface":
+                    view.add_representation(
+                        "surface",
+                        surfaceType="av",
+                        probeRadius=2.1,
+                        component=0,
+                        color=scheme,
+                    )
+                else:
+                    view.add_representation(query_repr, component=0, color=scheme)
+                view.add_representation(database_repr, component=1, color=scheme)
+                view.center(component=0)
 
-    return view
+        os.remove("temp.pdb")
+        os.remove("temp2.pdb")
+
+    else:
+        view = nv.NGLWidget()
+        view.add_trajectory(nv.MDAnalysisTrajectory(universe.query))
+        view.add_trajectory(nv.MDAnalysisTrajectory(universe.database))
+        view.clear_representations(component=0)
+        view.clear_representations(component=1)
+        if query_repr == "surface":
+            view.add_representation(
+                "surface", surfaceType="av", probeRadius=2.1, component=0, color=scheme
+            )
+        else:
+            view.add_representation(query_repr, component=0, color=scheme)
+        view.add_representation(database_repr, component=1, color=scheme)
+        view.center(component=0)
+
+    return view, scheme
