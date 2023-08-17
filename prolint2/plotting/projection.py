fdccffa25e470cd66d58c16498c924efb07ef9c7
Daniel Pastor Ramirez <daniel.ramirezecheme@ucalgary.ca>
2023-08-17 10:08:44 -0600

    projection prototype 2

--- prolint2/plotting/projection.py
+++ prolint2/plotting/projection.py
@@ -10,10 +10,10 @@ def show_contact_projection(
     metric,
     metric_name=None,
     lipid=None,
-    ngl_repr="surface",
+    query_repr="surface",
+    database_repr="spacefill",
     cmap="Reds",
     frame_idx=None,
-    show_database=False,
 ):
     metric_list = get_metric_list_by_residues(
         universe, metric, lipid=lipid, metric_name=metric_name
@@ -21,60 +21,55 @@ def show_contact_projection(
 
     metric_dict = {
         res_id: metric_list[idx]
-        for idx, res_id in enumerate(universe.query.residues.resids)
+        for idx, res_id in enumerate(universe.query.residues.resindices)
     }
 
-    atomic_bfactors = [metric_dict[x] for x in universe.query.atoms.resids]
+    max_metric = max(metric_list)
+    dat_resnames = universe.database.residues.resnames
+    metric_dist2 = {
+        res_id: max_metric
+        if dat_resnames[idx] == lipid else 0
+        for idx, res_id in enumerate(universe.database.residues.resindices)
+    }
+    
+    metric_dict.update(metric_dist2)
+
+    atomic_bfactors = [metric_dict[x] for x in universe.atoms.resindices]
 
     bf_cmap = cm.get_cmap(cmap)
 
     colors = [mpl.colors.to_hex(x) for x in bf_cmap(shift_range(atomic_bfactors))]
-    cs = [[y, str(universe.query.atoms.resids[x])] for x, y in enumerate(colors)]
+    cs = [[y, str(universe.atoms.resindices[x])] for x, y in enumerate(colors)]
 
     scheme = nv.color._ColorScheme(cs, "bf")
 
-    reprs = [
-        "point",
-        "line",
-        "rope",
-        "tube",
-        "trace",
-        "label",
-        "cartoon",
-        "licorice",
-        "ribbon",
-        "backbone",
-        "spacefill",
-        "ball+stick",
-    ]
-
-    params = {
-        "surface": [
-            {
-                "type": "surface",
-                "params": {"surfaceType": "av", "probeRadius": 2.1, "color": scheme},
-            }
-        ],
-    }
-
-    for rep in reprs:
-        params[rep] = [{"type": rep, "params": {"color": scheme}}]
-
-    if frame_idx != None:
+    if frame_idx is None:
+        universe.trajectory[0]
+    else:
         universe.trajectory[frame_idx]
-        universe.query.atoms.write("temp.pdb")
+    universe.query.atoms.write("temp.pdb")
+    universe.database.atoms.write("temp2.pdb")
 
-        with open("temp.pdb", "r") as myfile:
+    with open("temp.pdb", "r") as myfile:
+        with open("temp2.pdb") as myfile2:
             query_structure = nv.TextStructure(myfile.read())
+            database_structure = nv.TextStructure(myfile2.read())
             view = nv.NGLWidget()
             view.add_component(query_structure)
-            view.set_representations(params[ngl_repr])
+            view.add_component(database_structure)
+            view.clear_representations(component=0)
+            view.clear_representations(component=1)
+            if query_repr == "surface":
+                view.add_representation("surface", surfaceType="av", probeRadius=2.1, component=0, color=scheme)
+            else:
+                view.add_representation(query_repr, component=0, color=scheme)
+            view.add_representation(database_repr, component=1, color=scheme)
+            view.center(component=0)
 
-        # close file and remove it
-        myfile.close()
-        os.remove("temp.pdb")
-    else:
-        view = nv.show_mdanalysis(universe.query)
-        view.set_representations(params[ngl_repr])
+    # close file and remove it
+    myfile.close()
+    myfile2.close()
+    os.remove("temp.pdb")
+    os.remove("temp2.pdb")
 
     return view
